fd11dc4932896883c35df24b36977e60b2ea544b
Charanaraj N <goudacharanraj760@gmail.com>
2023-05-15 12:40:29 +0530

    feat - Add new task'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import React, { Fragment, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [todos, setTodos] = useState([
+    "Learn React",
+    "Learn Javascript",
+    "Learn CSS",
+    "Learn HTML",
+  ]);
+
+  const [inputTodo, setInputTodo] = useState("");
+
+  const handleAddTask = () => {
+    setTodos([inputTodo, ...todos]); //re-arranging
+    setInputTodo(""); //resetting the input
+  };
+
+  return (
+    <Fragment>
+      <input
+        type="text"
+        onChange={(e) => setInputTodo(e.target.value)}
+        value={inputTodo}
+      />
+      <button onClick={handleAddTask}>Add</button>
+      {todos.map((todo, index) => (
+        <p key={index}>{todo}</p>
+      ))}
+    </Fragment>
+  );
+};
 
 export default App;
